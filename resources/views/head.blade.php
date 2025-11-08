5d9fa2a11da811ebe877795b2fa8c64c82f63572
jananibidar@gmail.com <zilitechy@gmail.com>
2025-11-08 14:02:33 +0530

    admin page updated

---
+++ resources/views/head.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr" data-nav-layout="vertical" data-theme-mode="light" data-header-styles="light" data-menu-styles="light" data-toggled="close">
+
+<head>
+
+    <!-- Meta Data -->
+    <meta charset="UTF-8">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title> Valex - Bootstrap 5 Premium Admin & Dashboard Template </title>
+    <meta name="Description" content="Bootstrap Responsive Admin Web Dashboard HTML5 Template">
+    <meta name="Author" content="Spruko Technologies Private Limited">
+	<meta name="keywords" content="admin dashboard template,admin panel html,bootstrap dashboard,admin dashboard,html template,template dashboard html,html css,bootstrap 5 admin template,bootstrap admin template,bootstrap 5 dashboard,admin panel html template,dashboard template bootstrap,admin dashboard html template,bootstrap admin panel,simple html template,admin dashboard bootstrap">
+
+
+    
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('images/brand-logos/favicon.ico') }}" type="image/x-icon">
+    
+    <!-- Choices JS -->
+    <script src="{{ asset('libs/choices.js/public/assets/scripts/choices.min.js') }}"></script>
+
+    <!-- Main Theme Js -->
+    <script src="{{ asset('js/main.js') }}"></script>
+    
+    <!-- Bootstrap Css -->
+    <link id="style" href="{{ asset('libs/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" >
+
+    <!-- Style Css -->
+    <link href="{{ asset('css/styles.min.css') }}" rel="stylesheet" >
+
+    <!-- Icons Css -->
+    <link href="{{ asset('css/icons.css') }}" rel="stylesheet" >
+
+    <!-- Node Waves Css -->
+    <link href="{{ asset('libs/node-waves/waves.min.css') }}" rel="stylesheet" > 
+
+    <!-- Simplebar Css -->
+    <link href="{{ asset('libs/simplebar/simplebar.min.css') }}" rel="stylesheet" >
+    
+    <!-- Color Picker Css -->
+    <link rel="stylesheet" href="{{ asset('libs/flatpickr/flatpickr.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('libs/@simonwep/pickr/themes/nano.min.css') }}">
+
+
+
+    <!-- Choices Css -->
+    <link rel="stylesheet" href="{{ asset('libs/choices.js/public/assets/styles/choices.min.css') }}">
+
+
+
+<!-- Jsvector Maps -->
+<link rel="stylesheet" href="{{ asset('libs/jsvectormap/css/jsvectormap.min.css') }}">
+
+</head>
